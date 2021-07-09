6a593169e76f6fb73865074ce8e73c2c5e1e4989
Konstantin BIFERT <konstantin.b@worklife.io>
2021-07-09 09:52:15 +0200

    working

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   future: {
+//     webpack5: true,
+//   },
+//   webpack: (config, options) => {
+//     config.module.rules.push({
+//       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//       type: 'asset/resource',
+//     });
+//     return config
+//   },
+// };
+
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import riskyImage from '/public/arrow.png'
 
 export default function Home() {
   return (
@@ -8,56 +11,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div>
+        <p>next/image</p>
+        <Image
+          src={riskyImage}
+          alt="Picture of the author"
+          width={500}
+          height={500} />
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Image
+          src="https://images.unsplash.com/photo-1606787619248-f301830a5a57"
+          alt="Picture of the author"
+          width={500}
+          height={500} />
+      </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -204,6 +172,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+    </div >
   )
 }
